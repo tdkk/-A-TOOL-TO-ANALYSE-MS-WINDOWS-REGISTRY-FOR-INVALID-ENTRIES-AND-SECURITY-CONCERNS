cd652ef16659f67888a48265a611b6be721ffb06
tdkk <84286655+tdkk@users.noreply.github.com>
2025-06-13 07:30:16 +0100

    Delete reportWriter.h

--- reportWriter.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include <windows.h>
-#include <vector>
-#include "registryScanner.h"
-
-void writeReport(const std::vector<RegistryEntry>& entries, const std::string& filename);
-void writeFlaggedEntries(const std::vector<RegistryEntry>& entries, const std::string& filename);
